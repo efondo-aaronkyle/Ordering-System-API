9cfe81f7586197cffc6987bde84cd4292775af95
efondo-aaronkyle <akyleefondo@gmail.com>
2026-06-09 17:22:45 +0800

    fix customer request DTO

--- src/main/java/com/ordermanagement/dto/CustomerRequestDTO.java
+++ src/main/java/com/ordermanagement/dto/CustomerRequestDTO.java
@@ -1,21 +1,15 @@
 package com.ordermanagement.dto;
 
-import java.time.LocalDateTime;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class CustomerRequestDTO {
-	private Long id;
+	
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String phone;
-	private LocalDateTime createdAt;
+	
 }
