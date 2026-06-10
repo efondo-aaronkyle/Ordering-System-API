d7261fd7f7f7fc14d0e86200fc49b984c8a69390
efondo-aaronkyle <akyleefondo@gmail.com>
2026-06-10 14:39:01 +0800

    add response DTO

---
+++ src/main/java/com/ordermanagement/dto/SalesReportResponseDTO.java
@@ -0,0 +1,19 @@
+package com.ordermanagement.dto;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SalesReportResponseDTO {
+	
+	private Long totalOrders;
+	private BigDecimal totalRevenue;
+	
+}
