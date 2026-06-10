1f7922cc42a67472a874236882a9cb3a1886aed1
althenotero <ishigamisenku8888@gmail.com>
2026-06-10 13:43:08 +0800

    Implement reporting API for sales and top products

--- src/main/java/com/ordermanagement/repository/ReportMapper.java
+++ src/main/java/com/ordermanagement/repository/ReportMapper.java
@@ -1,8 +1,14 @@
 package com.ordermanagement.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.ordermanagement.dto.SalesReportResponseDTO;
+import com.ordermanagement.dto.TopProductResponseDTO;
 
 @Mapper
 public interface ReportMapper {
-
+	SalesReportResponseDTO getSalesReport();
+	List<TopProductResponseDTO> getTopSellingProducts();
 }
